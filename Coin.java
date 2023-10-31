877bb12fc479e8818935942e866f635949e28882
Ana Teixeira <pauloteixeira@Air-de-Ana.Home>
2023-10-31 11:45:21 +0000

    Runned the program with 40 iterations

--- Coin.java
+++ Coin.java
@@ -6,21 +6,19 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 import java.util.concurrent.RecursiveTask;
 
-public class Coin extends RecursiveTask<Integer> implements Runnable {
+public class Coin extends RecursiveTask<Integer>{
 
 	private int[] coins;
 	private int index;
 	private int accumulator;
-	private int depth;
 	private int strategy;	
 	public static final int LIMIT = 999;
 
-	public Coin(int[] coins, int index, int accumulator,  int depth, int strategy) {
+	public Coin(int[] coins, int index, int accumulator, int strategy) {
 
 		this.coins = coins;
 		this.index = index;
 		this.accumulator = accumulator;
-		this.depth = depth;
 		this.strategy = strategy;
 	}
 	
@@ -52,8 +50,8 @@ public class Coin extends RecursiveTask<Integer> implements Runnable {
 		return Math.max(a,  b);
 	}
 	
-	private static int par(int[] coins, int index, int accumulator, int depth, int strategy) {
-		Coin f2 = new Coin(coins, index, accumulator, depth, strategy);
+	private static int par(int[] coins, int index, int accumulator, int strategy) {
+		Coin f2 = new Coin(coins, index, accumulator, strategy);
 		return f2.compute();
 	}
 
@@ -85,33 +83,45 @@ public class Coin extends RecursiveTask<Integer> implements Runnable {
 			case 2:
 				if (RecursiveTask.getSurplusQueuedTaskCount() > 4 ) return seq(coins, index, accumulator);
 				break;
+
 			case 3:
-				if (depth >= 20) return seq(coins, index, accumulator);
+				if (index >= 15) return seq(coins, index, accumulator);
 				break;
 
 			case 4:
-				if (depth >= 25) return seq(coins, index, accumulator);
+				if (index >= 20) return seq(coins, index, accumulator);
 				break;
+
+			// case 5:
+			// 	if (index >= 25) return seq(coins, index, accumulator);
+			// 	break;
 
 			case 5:
 				// Max tasks: if the total number of tasks >= 2 * #cores.
 				// System.out.println("\n task count = " + Coin.getQueuedTaskCount());
 				// System.out.println("\n parallelism: " + Coin.getPool().getParallelism());
-				if (Coin.getQueuedTaskCount() > 3 * Coin.getPool().getParallelism()) return seq(coins, index, accumulator);
+				if (Coin.getQueuedTaskCount() > 2 * Coin.getPool().getParallelism()) return seq(coins, index, accumulator);
 				break;
+			// case 6:
+			// 	// Max tasks: if the total number of tasks >= 2 * #cores.
+			// 	// System.out.println("\n task count = " + Coin.getQueuedTaskCount());
+			// 	// System.out.println("\n parallelism: " + Coin.getPool().getParallelism());
+			// 	if (Coin.getQueuedTaskCount() > 3 * Coin.getPool().getParallelism()) return seq(coins, index, accumulator);
+			// 	break;
 
-			case 6:
-				// Max tasks: if the total number of tasks >= 3 * #cores.
-				if (Coin.getQueuedTaskCount() > 4 * Coin.getPool().getParallelism()) return seq(coins, index, accumulator);
-				break;
+			// case 6:
+			// 	// Max tasks: if the total number of tasks >= 3 * #cores.
+			// 	if (Coin.getQueuedTaskCount() > 4 * Coin.getPool().getParallelism()) return seq(coins, index, accumulator);
+			// 	break;
 
 			default:
 				return seq(coins, index, accumulator);
 		}
 
-		Coin f1 = new Coin(coins, index+1, accumulator, depth+1, strategy);
+		// System.out.println("Index: " + index);
+		Coin f1 = new Coin(coins, index+1, accumulator,  strategy);
 		f1.fork();
-		Coin f2 = new Coin(coins, index+1, accumulator + coins[index], depth+1, strategy);
+		Coin f2 = new Coin(coins, index+1, accumulator + coins[index], strategy);
 		f2.fork();
 
 		int a = f1.join();
@@ -119,8 +129,7 @@ public class Coin extends RecursiveTask<Integer> implements Runnable {
 		return Math.max(a,  b);
 	}
 
-	@Override
-	public void run() {
+	public static void main(String[] args) {
 		int nCores = Runtime.getRuntime().availableProcessors();
 
 		int[] coins = createRandomCoinSet(30);
@@ -128,7 +137,7 @@ public class Coin extends RecursiveTask<Integer> implements Runnable {
 		try {
 			FileWriter csvWriter = new FileWriter("results.csv");
 
-			int repeats = 2;
+			int repeats = 40;
 			
 			List<Integer> threads = new ArrayList<>();
 			int j = 2;
@@ -142,10 +151,9 @@ public class Coin extends RecursiveTask<Integer> implements Runnable {
 				header.append(", Surplus Tuning 2:Thread " + t);
 				header.append(", Surplus Tuning 3:Thread " + t);
 				header.append(", Surplus Tuning 4:Thread " + t);
-				header.append(", Depth Tuning 20:Thread " + t);
-				header.append(", Depth Tuning 25:Thread " + t);
-				header.append(", MaxTasks Tuning 3:Thread " + t);
-				header.append(", MaxTasks Tuning 4:Thread " + t);
+				header.append(", Index Tuning 15:Thread " + t);
+				header.append(", Index Tuning 20:Thread " + t);
+				header.append(", MaxTasks Tuning 2:Thread " + t);
 			}
 			csvWriter.write(header.toString() + "\n");
 
@@ -153,10 +161,10 @@ public class Coin extends RecursiveTask<Integer> implements Runnable {
 			strategies.put(0, "Surplus Tuning 2");
 			strategies.put(1, "Surplus Tuning 3");
 			strategies.put(2, "Surplus Tuning 4");
-			strategies.put(3, "Depth Tuning 20");
-			strategies.put(4, "Depth Tuning 25");
-			strategies.put(5, "MaxTasks Tuning 3");
-			strategies.put(6, "MaxTasks Tuning 4");
+			strategies.put(3, "Index Tuning 15");
+			strategies.put(4, "Index Tuning 20");
+			strategies.put(5, "MaxTasks Tuning 2");
+			// strategies.put(6, "MaxTasks Tuning 3");
 
 			for (int i=0; i<repeats; i++) {
 
@@ -172,7 +180,7 @@ public class Coin extends RecursiveTask<Integer> implements Runnable {
             		ForkJoinPool pool = new ForkJoinPool(currentThread);
 					for (int s : strategies.keySet()) {				
 						long parInitialTime = System.nanoTime();
-						Future<Integer> future = pool.submit(() -> par(coins, 0, 0, 0, s));
+						Future<Integer> future = pool.submit(() -> par(coins, 0, 0, s));
 						int rp = future.get();
 						long parEndTime = System.nanoTime() - parInitialTime;
 						System.out.println(currentThread + ";Strategy:" + strategies.get(s) + ";" + parEndTime);
--- stats.py
+++ stats.py
@@ -2,7 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('results.csv')
+
+# surplus_strategy = df.loc[:, df.columns.str.contains('Surplus')]
+# print(surplus_strategy)
+
+# plt.boxplot(surplus_strategy, labels=surplus_strategy.columns)
+# plt.xticks(fontsize=6)
+# plt.xticks(rotation=35)
+# plt.show()
+
+# Exibir o boxplot com todas as colunas
 plt.boxplot(df, labels=df.columns)
-plt.xticks(fontsize=8)
-plt.xticks(rotation=45)
+plt.xticks(fontsize=6)
+plt.xticks(rotation=35)
 plt.show()
