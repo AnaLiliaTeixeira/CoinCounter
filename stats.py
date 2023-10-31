906d6ec91a6961e6abe80e5843b8fd249d03e1e0
Ana Teixeira <pauloteixeira@Air-de-Ana.fc.ul.pt>
2023-10-31 14:14:17 +0000

    Best tuning for each srategy found and compared with each other

--- stats.py
+++ stats.py
@@ -3,16 +3,39 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('results.csv')
 
-# surplus_strategy = df.loc[:, df.columns.str.contains('Surplus')]
-# print(surplus_strategy)
+surplusStrategy = df.loc[:, df.columns.str.contains('Surplus')]
+plt.boxplot(surplusStrategy, labels=surplusStrategy.columns)
+plt.xticks(fontsize=6)
+plt.xticks(rotation=35)
+plt.show()
 
-# plt.boxplot(surplus_strategy, labels=surplus_strategy.columns)
-# plt.xticks(fontsize=6)
-# plt.xticks(rotation=35)
-# plt.show()
+bestSurplusTuning = surplusStrategy.mean().idxmin()
+print("Best tuning for Surplus strategy:", bestSurplusTuning)
 
-# Exibir o boxplot com todas as colunas
-plt.boxplot(df, labels=df.columns)
+indexStrategy = df.loc[:, df.columns.str.contains('Index')]
+plt.boxplot(indexStrategy, labels=indexStrategy.columns)
+plt.xticks(fontsize=6)
+plt.xticks(rotation=35)
+plt.show()
+
+bestIndexTuning = indexStrategy.mean().idxmin()
+print("Best tuning for Index strategy:", bestIndexTuning)
+
+maxTasksStrategy = df.loc[:, df.columns.str.contains('MaxTasks')]
+plt.boxplot(maxTasksStrategy, labels=maxTasksStrategy.columns)
+plt.xticks(fontsize=6)
+plt.xticks(rotation=35)
+plt.show()
+
+bestMaxTasksTuning = maxTasksStrategy.mean().idxmin()
+print("Best tuning for Index strategy:", bestMaxTasksTuning)
+
+new_df = pd.DataFrame({'Sequential': df['Sequential'], 
+                       bestSurplusTuning: df[bestSurplusTuning], 
+                       bestIndexTuning: df[bestIndexTuning], 
+                       bestMaxTasksTuning: df[bestMaxTasksTuning]})
+
+plt.boxplot(new_df, labels=new_df.columns)
 plt.xticks(fontsize=6)
 plt.xticks(rotation=35)
 plt.show()
